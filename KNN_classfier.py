1b39a44f5ca6237b73f192b84cec2d1ce5227170
im-minion <vaibhavminiyar@gmail.com>
2017-12-12 16:13:27 +0530

    KNN

---
+++ KNN_classfier.py
@@ -0,0 +1,7 @@
+# larger the dataset worst this algo goes
+# training and testing same
+# scaling not good
+# SVM is better than than KNN
+
+# KNN is fine for GB's of data and its not for billions of data point
+
