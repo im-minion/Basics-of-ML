0eaee165e9b4c59ab9f6d3afeb15fde170e87cca
im-minion <vaibhavminiyar@gmail.com>
2017-12-13 00:49:52 +0530

    14Done

--- KNN_classfier.py
+++ KNN_classfier.py
@@ -5,3 +5,40 @@
 
 # KNN is fine for GB's of data and its not for billions of data point
 
+'''
+   1. Sample code number            id number
+   2. Clump Thickness               1 - 10
+   3. Uniformity of Cell Size       1 - 10
+   4. Uniformity of Cell Shape      1 - 10
+   5. Marginal Adhesion             1 - 10
+   6. Single Epithelial Cell Size   1 - 10
+   7. Bare Nuclei                   1 - 10
+   8. Bland Chromatin               1 - 10
+   9. Normal Nucleoli               1 - 10
+  10. Mitoses                       1 - 10
+  11. Class:                        (2 for benign, 4 for malignant)  
+'''
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors
+import pandas as pd
+
+df =  pd.read_csv('breast-cancer-wisconsin.data')
+df.replace('?',-99999, inplace=True)
+df.drop(['id'],1,inplace=True) #dropping id column as it is outlier
+
+X = np.array(df.drop(['class'],1))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size = 0.2)
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
+
+example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,1,2,1,3,2,1]])
+example_measures = example_measures.reshape(len(example_measures),-1)
+prediction = clf.predict(example_measures)
+print(prediction)
