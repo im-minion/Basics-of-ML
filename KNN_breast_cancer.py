1a69ecc98cafdcfebc938c528c659c2bdd1802cb
im-minion <vaibhavminiyar@gmail.com>
2017-12-13 20:51:17 +0530

    19DONE

--- KNN_breast_cancer.py
+++ KNN_breast_cancer.py
@@ -17,40 +17,48 @@ def k_nearest_neighbors(data, predict, k=3):
 			ecludein_distance = np.linalg.norm(np.array(features) - np.array(predict))
 			distances.append([ecludein_distance, group])
 	votes = [i[1] for i in sorted(distances)[:k]]
-	print(Counter(votes).most_common(1))
+	# print(Counter(votes).most_common(1))
 	vote_result = Counter(votes).most_common(1)[0][0]
+	confidence = Counter(votes).most_common(1)[0][1] / k
+	
+	# print(vote_result, confidence)
 
-	return vote_result
+	return vote_result, confidence
+accuracies = []
+for i in range(25):
+	df = pd.read_csv('breast-cancer-wisconsin.data')
+	df.replace('?',-99999,inplace=True)
+	df.drop(['id'],1,inplace=True)
+	# print(df.head())
+	full_data = df.astype(float).values.tolist()
+	random.shuffle(full_data)
+	test_size = 0.4
 
-df = pd.read_csv('breast-cancer-wisconsin.data')
-df.replace('?',-99999,inplace=True)
-df.drop(['id'],1,inplace=True)
-# print(df.head())
-full_data = df.astype(float).values.tolist()
-random.shuffle(full_data)
-test_size = 0.2
+	train_set = {2:[], 4:[]}
+	test_set = {2:[], 4:[]}
 
-train_set = {2:[], 4:[]}
-test_set = {2:[], 4:[]}
+	train_data = full_data[:-int(test_size*len(full_data))]
+	test_data = full_data[-int(test_size*len(full_data)):]
+	# last 20% will be test data
 
-train_data = full_data[:-int(test_size*len(full_data))]
-test_data = full_data[-int(test_size*len(full_data)):]
-# last 20% will be test data
+	for i in train_data:
+		train_set[i[-1]].append(i[:-1])
 
-for i in train_data:
-	train_set[i[-1]].append(i[:-1])
+	for i in test_data:
+		test_set[i[-1]].append(i[:-1])
 
-for i in test_data:
-	test_set[i[-1]].append(i[:-1])
+	correct = 0
+	total = 0
 
-correct = 0
-total = 0
+	for gropu in test_set:
+		for data in test_set[gropu]:
+			vote, confidence = k_nearest_neighbors(train_set,data,k=5)
+			if gropu == vote:
+				correct +=1
+			# else:
+			# 	print(confidence)
+			total +=1
+	print('Accuracy :',correct/total)
+	accuracies.append(correct/total)
 
-for gropu in test_set:
-	for data in test_set[gropu]:
-		vote = k_nearest_neighbors(train_set,data,k=5)
-		if gropu == vote:
-			correct +=1
-		total +=1
-
-print('Accuracy:',correct/total)
+print('Accuracies : ',sum(accuracies)/len(accuracies))
--- KNN_classfier.py
+++ KNN_classfier.py
@@ -3,7 +3,7 @@
 # scaling not good
 # SVM is better than than KNN
 
-# KNN is fine for GB's of data and its not for billions of data point
+# KNN is fine for GB's of data and its not for TB's or billions of data point
 
 '''
    1. Sample code number            id number
@@ -22,23 +22,27 @@ import numpy as np
 from sklearn import preprocessing, cross_validation, neighbors
 import pandas as pd
 
-df =  pd.read_csv('breast-cancer-wisconsin.data')
-df.replace('?',-99999, inplace=True)
-df.drop(['id'],1,inplace=True) #dropping id column as it is outlier
+accuracies = []
+for  i in range(25):
+  df =  pd.read_csv('breast-cancer-wisconsin.data')
+  df.replace('?',-99999, inplace=True)
+  df.drop(['id'],1,inplace=True) #dropping id column as it is outlier
 
-X = np.array(df.drop(['class'],1))
-y = np.array(df['class'])
+  X = np.array(df.drop(['class'],1))
+  y = np.array(df['class'])
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size = 0.2)
+  X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size = 0.2)
 
-clf = neighbors.KNeighborsClassifier()
-clf.fit(X_train, y_train)
+  clf = neighbors.KNeighborsClassifier()
+  clf.fit(X_train, y_train)
 
-accuracy = clf.score(X_test, y_test)
+  accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+  print('Accuracy : ',accuracy)
+  accuracies.append(accuracy)
+  # example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,1,2,1,3,2,1]])
+  # example_measures = example_measures.reshape(len(example_measures),-1)
+  # prediction = clf.predict(example_measures)
+  # print(prediction)
 
-example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,1,2,1,3,2,1]])
-example_measures = example_measures.reshape(len(example_measures),-1)
-prediction = clf.predict(example_measures)
-print(prediction)
+print('Accuracies : ',sum(accuracies)/len(accuracies))
